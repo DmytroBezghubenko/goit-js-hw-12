ba2988fa831fbeff5417c4b00a4eb1fe60e13201
DmytroBezghubenko <dimon14032@gmail.com>
2024-04-12 22:44:28 +0300

    *

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,9 @@
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "43225005-d66d61a579372833bf726388e";
 
-export function fetchImages(nameImg) {
+import axios from "axios";
+
+export async function fetchImages(nameImg) {
     const params = new URLSearchParams({
         key: API_KEY,
         q: nameImg,
@@ -10,11 +12,11 @@ export function fetchImages(nameImg) {
         safesearch: true,
     });
 
-    return fetch(`${BASE_URL}?${params}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Ooooops");
-            }
-            return response.json();
-        });
+    try {
+        const response = await axios.get(`${BASE_URL}?${params}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching images:", error);
+        throw error;
+    }
 }
